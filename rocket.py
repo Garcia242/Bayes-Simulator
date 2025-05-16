f58f54913b4a8bcbcafa320a19292d67bc313840
Garcia242 <70655945+Garcia242@users.noreply.github.com>
2025-05-16 15:41:05 +0100

    first commit

---
+++ rocket.py
@@ -0,0 +1,6 @@
+class rocket
+    def __init__(self, ):
+        self.burn = burn #burn time 
+        self.thrust = thrust #thrust
+        self.mass = mass #mass
+        self.fuel_mass = fuel_mass #fuel mass
