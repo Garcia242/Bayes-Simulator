9d13b73503e1325c9a4bfe7d75974af56eeeb286
Garcia242 <70655945+Garcia242@users.noreply.github.com>
2025-05-17 15:07:33 +0100

    Dowloading RocketPy Examples

--- rocket.py
+++ rocket.py
@@ -1,6 +1,10 @@
+import rocketpy
+
 class rocket
-    def __init__(self, ):
+    def __init__(self, burn, thrust, mass, fuel_mass):
+
         self.burn = burn #burn time 
         self.thrust = thrust #thrust
         self.mass = mass #mass
         self.fuel_mass = fuel_mass #fuel mass
+        
