105613d2dd48969d14a8af4e8f49c59a1ff32c6d
Garcia242 <70655945+Garcia242@users.noreply.github.com>
2025-05-19 15:34:46 +0100

    trying to create a rocket class - rocketpy may not be the answer or at least not for now

--- rocket.py
+++ rocket.py
@@ -1,10 +1,22 @@
 import rocketpy
 
 class rocket
-    def __init__(self, burn, thrust, mass, fuel_mass):
+    def __init__(self, burn, thrust, mass, fuel_mass, fin_area, nose_area):
 
         self.burn = burn #burn time 
         self.thrust = thrust #thrust
         self.mass = mass #mass
         self.fuel_mass = fuel_mass #fuel mass
+        self.fin_area = fin_area #area for the fins
+        self.nose_area = nose_area #area for the nose cone 
+        self.drag_coefficient = 0.5 #drag coefficient
+        
+    def position_init(self, x, y, z, vx, vy, vz):
+        # Initialize the position and velocity of the rocket
+        # Calculate the position of the rocket
+        self.pos = [x, y, z]
+        self.vel = [vx, vy, vz]
+    return x , y, z
+
+
         
